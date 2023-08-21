71f01a8718ebb857e8b0f3d0da56575972934a55
rstevens198 <ryanstevens198@yahoo.com>
2023-08-21 15:02:06 -0700

    added in mongoose to connect to our database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
