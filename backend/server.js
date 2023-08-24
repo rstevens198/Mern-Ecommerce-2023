357af4b0d3099763a08e57e61c3e2da0b763e420
rstevens198 <ryanstevens198@yahoo.com>
2023-08-23 22:21:55 -0700

    added in custom error messages

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productsRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -14,5 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productsRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
