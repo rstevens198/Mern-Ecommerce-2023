c135bada63d91ed0f452e59c224ff08927e7b522
rstevens198 <ryanstevens198@yahoo.com>
2023-09-01 20:24:11 -0700

    login and logout

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,16 +1,22 @@
 import { USERS_URL } from "../constants.js";
 import { apiSlice } from "./apiSlice.js";
 
-export const usersApiSlice = apiSlice.injectEndpoints({
+export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: USERS_URL / "auth",
+        url: `${USERS_URL}/auth`,
         method: "POST",
         body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
       }),
     }),
   }),
 });
 
-export const { useLoginMutation } = usersApiSlice;
+export const { useLoginMutation, useLogoutMutation } = userApiSlice;
