ba0d5253d64acd7ef61d41732c918f4bf077619c
rstevens198 <ryanstevens198@yahoo.com>
2023-09-04 23:35:36 -0700

    paypal buttons

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -17,7 +17,7 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       keepUnsusedDataFor: 5,
     }),
     payOrder: builder.mutation({
-      query: (orderId, details) => ({
+      query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
         method: "PUT",
         body: { ...details },
