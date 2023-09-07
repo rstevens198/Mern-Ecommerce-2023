8c80b770de34fc180c6e16ba303f8e1d86df7873
rstevens198 <ryanstevens198@yahoo.com>
2023-09-06 22:32:27 -0700

    admin creating products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,24 @@ const getProductByID = asyncHandler(async (req, res) => {
   throw new Error("Product not found");
 });
 
-export { getProducts, getProductByID };
+// @description     Create a product
+// @route           POST /api/products
+// @access          Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReview: 0,
+    description: "Sample  description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+export { getProducts, getProductByID, createProduct };
