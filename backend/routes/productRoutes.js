4cfe7fe36b35986d5d8a94248843234b624ec1ed
rstevens198 <ryanstevens198@yahoo.com>
2023-09-07 00:02:09 -0700

    edit product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,11 @@ import {
   getProducts,
   getProductByID,
   createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductByID);
+router.route("/:id").get(getProductByID).put(protect, admin, updateProduct);
 
 export default router;
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,6 +1,6 @@
 import {LinkContainer} from "react-router-bootstrap"
 import {Table, Button, Row, Col} from "react-bootstrap"
-import {FaTimes, FaEdit, FaTrash} from "react-icons/fa"
+import { FaEdit, FaTrash} from "react-icons/fa"
 import {toast} from "react-toastify"
 import Message from "../../components/Message"
 import Loader from "../../components/Loader"
@@ -63,7 +63,7 @@ const ProductListScreen = () => {
                         <td>{product.category}</td>
                         <td>{product.brand}</td>
                         <td>
-                            <LinkContainer to={`admin/product/${product._id}/edit`}>
+                            <LinkContainer to={`/admin/product/${product._id}/edit`}>
                                 <Button variant="light" className="btn-sm mx-2">
                                     <FaEdit />
                                 </Button>
