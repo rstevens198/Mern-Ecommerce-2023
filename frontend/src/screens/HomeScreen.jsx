b11d4bf556af9f51c1186d2301bf1f7471cdb0d0
rstevens198 <ryanstevens198@yahoo.com>
2023-09-09 00:34:27 -0700

    search box components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from "react-bootstrap"
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 import Product from "../components/Product"
 import { useGetProductsQuery } from "../slices/productsApiSlice"
 import Loader from "../components/Loader"
@@ -18,6 +18,11 @@ const HomeScreen = () => {
   
   return (
     <>
+    {keyword && (
+      <Link to="/" className="btn btn-light mb-4">
+        Go Back
+      </Link>
+    )}
     {isLoading ? (
       <Loader />
     ) : error ? (<Message variant="danger">{ error?.data?.message || error.error}</Message>) : (<>
